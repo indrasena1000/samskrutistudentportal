672913edef39fbaeda62408dc513e21fd9af1de7
indra <indasena1000>
2020-10-27 23:39:58 +0530

    indra

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var cors = require('cors')
 const cookieParser = require('cookie-parser');
 var morgan = require('morgan')
 
-const flash = require('connect-flash');
+// const flash = require('connect-flash');
 const database = require("./models/index");
 //routes
 const studentRoute = require("./routes/studentRoute")
@@ -15,7 +15,7 @@ const { protect } = require('./middleware/auth');
 const app = express();
 app.use(express.json())
 
-app.use(flash());
+// app.use(flash());
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
 if (process.env.NODE_ENV === 'development') {
